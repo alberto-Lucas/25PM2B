549c38a7c1d9404986189865d59977db1ed542ef
Lucas Alberto <lucas.alberto@linx.com.br>
2025-08-26 20:44:29 -0300

    AppValidationLogin

---
+++ AppValidationLogin/Animation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppValidationLogin
+{
+    //Tranformar a classe em estatica
+    //pois não é nescessario realizar instancia
+    //de classe static
+    //Quando a classe é static, obrigatoriamente
+    //todos os seus métodos tambem devem ser
+    //static
+    public static class Animation
+    {
+        //Criar um método generico
+        //para aplicar animação de tremida
+        //em componenentes visuais (usuario ve)
+        static public async void Tremer(VisualElement elemento)
+        {
+            //Validar se o componente não é nullo
+            //ou seja se ele existe
+            if (elemento == null)
+                return; //abortar a rotina
+
+            //tempo padrão
+            uint tempo = 50;
+
+            //Lista de deslocamento (quantidade de movimentação)
+            var lista = new[] {-15, 15, -10, 10, -5, 5 };
+
+            //Loop na lista e a animação
+            foreach (var deslocamento in lista)
+            {
+                await elemento.TranslateTo(deslocamento, 0, tempo); ;
+            }
+            elemento.TranslationX = 0;
+        }
+    }
+}
---
+++ AppValidationLogin/Validation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppValidationLogin
+{
+    public class Validation
+    {
+        //Definir as propriedades referente
+        //ao nosso PAR de componentes
+        private Entry EntryCampo {  get; set; }
+        private Label LabelValidation { get; set; }
+
+        //Criar o construtor da classe
+        //para vincular os componentes as propriedades
+        public Validation(Entry txtCampo, Label lblValidation) 
+        { 
+            EntryCampo = txtCampo;
+            LabelValidation = lblValidation;
+        }
+    }
+}
