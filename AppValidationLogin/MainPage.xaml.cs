88995d2f3619f5006337fda90c657a0c834effb2
Lucas Alberto <lucas.alberto@linx.com.br>
2025-08-26 20:01:07 -0300

    AppValidationLogin

--- AppValidationLogin/MainPage.xaml.cs
+++ AppValidationLogin/MainPage.xaml.cs
@@ -7,9 +7,29 @@
             InitializeComponent();
         }
 
+        //Método para aplicar uma animação de tremida
+        //no campo entry
+        async void TremerEntry(Entry txtCampo)
+        {
+            //definir um tempo padrão
+            uint tempo = 50;
+
+            await txtCampo.TranslateTo(-15, 0, tempo);
+            await txtCampo.TranslateTo(15, 0, tempo);
+            await txtCampo.TranslateTo(-10, 0, tempo);
+            await txtCampo.TranslateTo(10, 0, tempo);
+            await txtCampo.TranslateTo(-5, 0, tempo);
+            await txtCampo.TranslateTo(5, 0, tempo);
+            txtCampo.TranslationX = 0;
+        }
+
         private void btnEntrar_Clicked(object sender, EventArgs e)
         {
+            if (txtEmail.Text != "admin")
+                TremerEntry(txtEmail);
 
+            if (txtSenha.Text != "admin")
+                TremerEntry(txtSenha);
         }
     }
 
