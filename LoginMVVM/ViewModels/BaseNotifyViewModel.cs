4a2dc80a6258f44e6ed96848ae1e68f1a3eb85cb
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2025-10-21 20:45:06 -0300

    LoginMVVM

---
+++ LoginMVVM/ViewModels/BaseNotifyViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LoginMVVM.ViewModels
+{
+    public class BaseNotifyViewModel : INotifyPropertyChanged
+    {
+        public event 
+            PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(
+            [CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke
+                (this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ LoginMVVM/ViewModels/BaseViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Java.Lang;
+using System.Windows.Input;
+
+namespace LoginMVVM.ViewModels
+{
+    public class BaseViewModel : BaseNotifyViewModel
+    {
+        //Command para acessar direto da View
+        public ICommand VoltarCommand { get; set; }
+        //Método comum para acessar direto do Backend
+        public async void Voltar()
+        {
+            await Application.Current.MainPage.
+                Navigation.PopAsync();
+        }
+        //Método comum para acessar direto do Backend
+        public async void AbrirView(ContentPage view)
+        {
+            await Application.Current.MainPage.
+                Navigation.PushAsync(view);
+        }
+
+        public async void MsgInfo(string conteudo)
+        {
+            await Application.Current.MainPage.
+                DisplayAlert("Informação...",
+                             conteudo,
+                             "OK");
+        }
+
+        //Quando utilizar async e esperar um tipo de dado
+        //como retorno, é preciso utilizar o Task
+        //Task<tipo_dado> ex: Task<int>....
+        public async Task<bool> MsgSimNao(string conteudo)
+        {
+            //A primeira sempre será a sim
+            //ou seja cliacado na primeira opção
+            //irá retornar true
+            //clicado na segunda retorna false
+            return 
+                await Application.Current.MainPage.
+                    DisplayAlert("Confirmação...",
+                                 conteudo,
+                                 "Sim", "Não");
+        }
+
+        public BaseViewModel()
+        {
+            VoltarCommand = new Command(Voltar);
+        }
+    }
+}
